f770c31482ef120a2c8879649c1eeaa9cd9453fb
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2020-06-26 23:03:43 +0530

    inserted a node in a linked list
    using the recursive method

---
+++ inset_node.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+node* insert_Node(node* head,int i,int data)
+{
+if(head==NULL)
+{
+return -1;
+}
+if (i==0)
+{
+node* newhead= new node(data);
+newhead->next=head;
+return newhead;
+}
+else{
+node* x=insert_Node(head+1,i-1,data);
+head->next=x;
+return head;
+}
+Node* takeinput() {
+    int data;
+    cin >> data;
+    Node* head = NULL, *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL)                  {
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node *head = takeinput();
+    int pos, data;
+    cin >> pos >> data;
+    head = insertNodeRec(head, pos, data);
+    print(head);
+    return 0;
+}
+
